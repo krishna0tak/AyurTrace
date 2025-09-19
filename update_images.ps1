75ae736b527a1e2217991bf8b8509fd446951e60
Krishna Tak <95999204+krishna0tak@users.noreply.github.com>
2025-09-19 19:06:55 +0530

    removed excess files

--- update_images.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem *.html | ForEach-Object {
-    $content = Get-Content $_.FullName
-    $content = $content -replace 'getImageFromPinata', 'getImageUrl'
-    Set-Content $_.FullName $content
-}
-Write-Host "Updated all HTML files to use getImageUrl instead of getImageFromPinata"
